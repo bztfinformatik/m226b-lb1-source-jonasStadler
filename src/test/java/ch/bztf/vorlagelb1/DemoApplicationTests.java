a0ccc0fc14a5bd006099f3efbe33c0ea26bc248f
jonasStadler <54017110+jonasStadler@users.noreply.github.com>
2020-06-19 19:13:51 +0200

    Update DemoApplicationTests.java

--- src/test/java/ch/bztf/vorlagelb1/DemoApplicationTests.java
+++ src/test/java/ch/bztf/vorlagelb1/DemoApplicationTests.java
@@ -1,9 +1,11 @@
 package ch.bztf.vorlagelb1;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import ch.bztf.vorlagelb1.model.Termin;
+import ch.bztf.Termine.model.*;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -12,10 +14,34 @@ class DemoApplicationTests {
 	void contextLoads() {
 	}
 
+	
+
+	//@DisplayName("Konstruktor test")
 	@Test
-	public void TerminTest(){
-		//Termin termin1 = new Termin("asd", "dfs", " Hoch");
-		//System.out.println(termin1 + "s");
+	public void BeschreibungTest(){
+		Termin termin1 = new Termin("37-20-2021", "Das ist ein Test", "Hoch");
+		assertEquals("Das ist ein Test", termin1.getBeschreibung());
 	}
+
+	@Test
+	public void DatumTest(){
+		Termin termin1 = new Termin("37-20-2021", "Das ist ein Test", "Hoch");
+		assertEquals("37-20-2021", termin1.getDatum());
+	}
+
+	@Test
+	public void wichtigkeitTest(){
+		Termin termin1 = new Termin("37-20-2021", "Das ist ein Test", "Hoch");
+		assertEquals("Hoch", termin1.getWichtigkeit());
+	}
+
+
+
+
+
+
+
+
+
 
 }
